5d20f40957edd1c9a3fe4cb42b38794cc6e4b8d6
Alvaro Volker <itsm.avolker.c@AES.COM>
2019-12-19 16:23:16 -0300

    curved navigation bar TO DO

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,44 +1,59 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return MyAppState();  
+  }
+}
 
-class HomePage extends StatelessWidget {
+
+
+class MyAppState extends State<HomePage>{
+
+  int _selectedPage = 0;
+  final _pageOptions = [
+  Text('Item 1'),
+  Text('Item 2'),
+  Text('Item 3')
+];
+
 
  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Card(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Text("Generación",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 22)
-                        ),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+        home: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedPage,
+            onTap: (int index){
+              setState(() {
+                _selectedPage = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home, color: (Colors.black)),
+                title: Text('Home', style: (
+                  TextStyle(color: Colors.black)
+                ),)
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Text("Favoritos",
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey,
-                        fontSize: 16)
-                        ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite, color: (Colors.black)),
+                title: Text('Favorites', style: (
+                  TextStyle(color: Colors.black)
+                ),textAlign: TextAlign.end,)
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                  child: Row(
-                children: <Widget>[
-                  Icon(Icons.person),
-                  Container(
-                    margin: EdgeInsets.only(left: 2),
-                  )
-                ],
-              )),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.graphic_eq, color: (Colors.black)),
+                title: Text('Graphic', style: (
+                  TextStyle(color: Colors.black)
+                ),)
+              ),
             ],
           ),
-        ));
+          ),
+    );    
   }
 }
