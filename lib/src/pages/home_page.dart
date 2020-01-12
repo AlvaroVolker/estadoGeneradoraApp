a45dcb19dbc447e1ab656843ca99db11d3893063
Alvaro volker <tolivolker13@gmail.com>
2020-01-11 23:04:58 -0300

    sad

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:estadogeneradoraapp/src/pages/content/bottomnavbar_homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'content/homepage/radialcirclechart_homepage.dart';
 
 class HomePage extends StatefulWidget {
