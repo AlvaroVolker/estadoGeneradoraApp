3bb954f2ef73dd353e00bac6e04f183792c5652a
Alvaro Volker <itsm.avolker.c@AES.COM>
2020-01-22 16:09:53 -0300

    login prueba

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,17 @@
 import 'package:estadogeneradoraapp/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
+      // routes: {
+      //   "/": (_) => new WebviewScaffold(
+      //     url: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+
+      //   )
+      // },
       debugShowCheckedModeBanner: false,
       home: Center(
         child: HomePage(),
