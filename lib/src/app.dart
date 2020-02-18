fd9cd3b388cb25508d981c1fb473328267da0b86
Alvaro Volker <avolker.admin@aesandes.com>
2020-02-18 15:50:40 -0300

    yea

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,6 @@
 import 'package:estadogeneradoraapp/src/core/routes.dart';
 import 'package:estadogeneradoraapp/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class MyApp extends StatelessWidget {
   @override
