1471c279613e80f7219298975cd907fdf570973c
Alvaro volker <tolivolker13@gmail.com>
2020-02-25 20:26:03 -0300

    dsa

--- lib/src/models/generacion.dart
+++ lib/src/models/generacion.dart
@@ -19,19 +19,4 @@ class DetalleGeneracion {
     this.listaDetalleGeneracion = new List<DetalleGeneracion>();
   }
 
-  DetalleGeneracion.fromJsonMap(Map<String, dynamic> json) {
-    id = json['Id'];
-    nombre = json['Nombre'].toString();
-    capacidadInstalada =
-        double.parse(json['CapacidadInstalada'].toString()).isNaN
-            ? 0
-            : double.parse(json['CapacidadInstalada'].toStringAsFixed(1));
-    capacidadUsada = !double.parse(json['CapacidadUsada'].toString()).isNaN
-        ? json['CapacidadUsada'].toStringAsFixed(0)
-        : "0";
-    generacionActual = double.parse(json['GeneracionActual'].toString()).isNaN
-        ? 0
-        : double.parse(json['GeneracionActual'].toStringAsFixed(1));
-    fechaActualizacion = json['FechaActualizacion'].toString();
-  }
 }
